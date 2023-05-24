24658b2e72d975d0ff95b87c69836ba5fbd3f54c
Nikhil Renjith <nikhilrenjith3@gmail.com>
2023-05-25 00:52:30 +0530

    Added decimal and other functions

--- calculator.js
+++ calculator.js
@@ -1,11 +1,14 @@
 function addinput(value) {
   document.getElementById("result").value += value;
 }
-
 function Clear() {
   document.getElementById("result").value = "";
 }
-
+function Del() {
+  var input = document.getElementById("result").value;
+  var newInput = input.slice(0, -1);
+  document.getElementById("result").value = newInput;
+}
 function find() {
   var input = document.getElementById("result").value;
   if (input === "") {
@@ -15,4 +18,9 @@ function find() {
     var result = eval(input);
     document.getElementById("result").value = result;
   }
+}
+function toggleSign() {
+  var input = document.getElementById("result").value;
+  var result = parseFloat(input) * -1;
+  document.getElementById("result").value = result;
 }
